da2732a9d526910360261442579e9ab913c344a8
Aman Acharya <165452540+aman-acharya@users.noreply.github.com>
2024-08-18 19:28:09 +0545

    updated engine for file format

--- pages/3_Gpt.py
+++ pages/3_Gpt.py
@@ -34,54 +34,56 @@ else:
                     except Exception as e:
                         pass
             elif uploaded_file.name.endswith('.xlsx'):
-                df = pd.read_excel(uploaded_file)
+                df = pd.read_excel(uploaded_file, engine='openpyxl')
             else:
                 st.error("Unsupported file format")
+        
+        
+            with st.expander("Show Dataframe"):
+                st.write(df)
+
+            # Create an agent for the dataframe
+            agent = create_pandas_dataframe_agent(ChatOpenAI(model=model, temperature=0, api_key=Api_Key, streaming=True, max_tokens=2000), df, verbose= True, allow_dangerous_code=True, max_iterations=3, agent_type="tool-calling", engine="pandas")
+
+            # Store LLM generated responses
+            if "messages" not in st.session_state.keys():
+                st.session_state.messages = []
+            # Display or clear chat messages
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.write(message["content"])
+
+            def clear_chat_history():
+            # clear the chat history
+                st.session_state.messages = []
+
+            st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+            # Create a text input for the user to enter their analysis prompt
+            prompt = st.chat_input("Enter your analysis prompt")
+                
+            if prompt is not None and prompt != "":
+                with st.container():
+                    st.spinner("Generating Response...")
+                    with st.chat_message("user"):
+                        st.markdown(prompt)
+                    st.session_state.messages.append({"role": "user", "content": prompt})
+
+                    # Generate a new response if last message is not from assistant
+                    if st.session_state.messages[-1]["role"] != "assistant":
+                        with st.spinner("Thinking..."):
+                            try:
+                                responses = agent(prompt)
+                            except ValueError:
+                                responses = {"output": "An error occurred. LLM could not generate a response. Please try again."}
+                        
+                            placeholder = st.empty()
+                        with st.chat_message("assistant"):
+                            st.write(responses['output'])
+                        message = {"role": "assistant", "content": responses['output']}
+                        st.session_state.messages.append(message)
+
         except Exception as e:
             st.error(f"Error loading file: {e}")
-        
-        with st.expander("Show Dataframe"):
-            st.write(df)
-
-        # Create an agent for the dataframe
-        agent = create_pandas_dataframe_agent(ChatOpenAI(model=model, temperature=0, api_key=Api_Key, streaming=True, max_tokens=2000), df, verbose= True, allow_dangerous_code=True, max_iterations=3, agent_type="tool-calling", engine="pandas")
-
-        # Store LLM generated responses
-        if "messages" not in st.session_state.keys():
-            st.session_state.messages = []
-        # Display or clear chat messages
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.write(message["content"])
-
-        def clear_chat_history():
-        # clear the chat history
-            st.session_state.messages = []
-
-        st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
-        # Create a text input for the user to enter their analysis prompt
-        prompt = st.chat_input("Enter your analysis prompt")
-            
-        if prompt is not None and prompt != "":
-            with st.container():
-                st.spinner("Generating Response...")
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-                st.session_state.messages.append({"role": "user", "content": prompt})
-
-                # Generate a new response if last message is not from assistant
-                if st.session_state.messages[-1]["role"] != "assistant":
-                    with st.spinner("Thinking..."):
-                        try:
-                            responses = agent(prompt)
-                        except ValueError:
-                            responses = {"output": "An error occurred. LLM could not generate a response. Please try again."}
-                    
-                        placeholder = st.empty()
-                    with st.chat_message("assistant"):
-                        st.write(responses['output'])
-                    message = {"role": "assistant", "content": responses['output']}
-                    st.session_state.messages.append(message)
     else:
         st.warning("Please upload a file to start the analysis.")
